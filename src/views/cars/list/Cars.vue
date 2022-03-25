eddd6e2be54139021d8f3d2014deff8278407b5e
Eposwiatny <eposwiatny@gmail.com>
2022-03-25 22:19:58 +0100

    Adding skeleton on car list

--- src/views/cars/list/Cars.vue
+++ src/views/cars/list/Cars.vue
@@ -1,25 +1,32 @@
 <template>
 <div>
     <h1 class="mb-5">Cars list</h1>
-
     <FilterBox />
 
-    <div class="row">
+    <div v-if="loadingStatus === 'LOADING'" class="row">
+        <SkeletonCarCard v-for="(car, index) in 4" :key="index" />
+    </div>
+
+    <div v-if="loadingStatus === 'OK'" class="row">
     <CarCard v-for="(car, index) in cars" :car="car" :key="index" />
     </div>
+
 </div>
 </template>
 <script>
+import 'vue-skeletor/dist/vue-skeletor.css';
 import FilterBox from '@/views/cars/list/components/FilterBox.vue'
+import SkeletonCarCard from '@/views/cars/list/components/SkeletonCarCard.vue'
 import CarCard from '@/views/cars/list/components/CarCard.vue'
 export default{
 
     name: 'car-card',
 
-    components: { CarCard, FilterBox },
+    components: { CarCard, FilterBox, SkeletonCarCard },
 
     data(){
         return{
+            loadingStatus: 'LOADING',
             cars:[
                 {
                     id: 1,
@@ -83,6 +90,16 @@ export default{
                 }
             ]
         }
+    },
+    methods:{
+        loadData(){
+            setTimeout(() => {
+                this.loadingStatus = 'OK'
+            }, 2300);
+        }
+    },
+    mounted(){
+        this.loadData()
     }
 }
 </script>
---
+++ src/views/cars/list/components/SkeletonCarCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-4 mb-3">
+    <div class="card">
+        <div class="card-body">
+            <div class="mb-2">
+                <div>
+                    <Skeletor class="mb-3" width="100%" height="16vw"/>
+                </div>
+            </div>
+                <Skeletor v-for="(el, index) in 6" :key="index" class="mb-3" width="200" height="14" pill/>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+import { Skeletor } from 'vue-skeletor';
+export default{
+
+    components: { Skeletor },
+
+}
+</script>
