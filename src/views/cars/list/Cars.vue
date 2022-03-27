b8e145d41f1c12d6ea82f34304a508da17c4c114
Eposwiatny <eposwiatny@gmail.com>
2022-03-27 12:30:27 +0200

    Retrieving data from DB

--- src/views/cars/list/Cars.vue
+++ src/views/cars/list/Cars.vue
@@ -7,6 +7,7 @@
         <SkeletonCarCard v-for="(car, index) in 4" :key="index" />
     </div>
 
+
     <div v-if="loadingStatus === 'OK'" class="row">
     <CarCard v-for="(car, index) in cars" :car="car" :key="index" />
     </div>
@@ -14,6 +15,7 @@
 </div>
 </template>
 <script>
+import axios from 'axios'
 import 'vue-skeletor/dist/vue-skeletor.css';
 import FilterBox from '@/views/cars/list/components/FilterBox.vue'
 import SkeletonCarCard from '@/views/cars/list/components/SkeletonCarCard.vue'
@@ -27,75 +29,17 @@ export default{
     data(){
         return{
             loadingStatus: 'LOADING',
-            cars:[
-                {
-                    id: 1,
-                    image: 'url',
-                    brand: 'Audi',
-                    model: 'A4',
-                    places: 4,
-                    transmission: 'Automatic',
-                    fuel: 'Diesel',
-                    price: 450
-                },
-                {
-                    id: 2,
-                    image: 'url',
-                    brand: 'BMW',
-                    model: 'Series 3',
-                    places: 5,
-                    transmission: 'Automatic',
-                    fuel: 'Gasoil',
-                    price: 450
-                },
-                {
-                    id: 3,
-                    image: 'url',
-                    brand: 'Mercedes',
-                    model: 'A45',
-                    places: 5,
-                    transmission: 'Manual',
-                    fuel: 'Gasoil',
-                    price: 450
-                },
-                {
-                    id: 1,
-                    image: 'url',
-                    brand: 'Audi',
-                    model: 'A4',
-                    places: 4,
-                    transmission: 'Automatic',
-                    fuel: 'Diesel',
-                    price: 450
-                },
-                {
-                    id: 2,
-                    image: 'url',
-                    brand: 'BMW',
-                    model: 'Series 3',
-                    places: 5,
-                    transmission: 'Automatic',
-                    fuel: 'Gasoil',
-                    price: 450
-                },
-                {
-                    id: 3,
-                    image: 'url',
-                    brand: 'Mercedes',
-                    model: 'A45',
-                    places: 5,
-                    transmission: 'Manual',
-                    fuel: 'Gasoil',
-                    price: 450
-                }
-            ]
+            cars:[]
         }
     },
     methods:{
         loadData(){
-            setTimeout(() => {
-                this.loadingStatus = 'OK'
-            }, 2300);
+            axios.get('http://192.168.0.42:8000/api/cars').then( response => {
+                this.cars = response.data.cars
+                setTimeout(() => {
+                        this.loadingStatus = 'OK'
+                }, 1000);
+            })
         }
     },
     mounted(){
--- src/views/cars/list/components/CarCard.vue
+++ src/views/cars/list/components/CarCard.vue
@@ -3,13 +3,13 @@
         <div class="card">
             <div class="card-body">
                 <div class="mb-2">
-                    <img class="img-thumbnail" src="https://ag-spots-2021.o.auroraobjects.eu/2021/07/04/audi-rs5-sportback-b9-c916204072021112424_1.jpg?1625390676" alt="">
+                    <img class="img-thumbnail" :src="car.img_url" alt="">
                 </div>
-                <div class="mb-1"><span class="fw-bold">Brand: </span>{{car.brand}}</div>
-                <div class="mb-1"><span class="fw-bold">Model: </span>{{car.model}}</div>
+                <div class="mb-1"><span class="fw-bold">Brand: </span>{{car.car_model.car_brand.name}}</div>
+                <div class="mb-1"><span class="fw-bold">Model: </span>{{car.car_model.name}}</div>
                 <div class="mb-1"><span class="fw-bold">Places: </span>{{`${car.places} places`}}</div>
-                <div class="mb-1"><span class="fw-bold">Transmission: </span>{{car.transmission}}</div>
-                <div class="mb-1"><span class="fw-bold">Fuel type: </span>{{car.fuel}}</div>
+                <div class="mb-1"><span class="fw-bold">Transmission: </span>{{car.is_automatic}}</div>
+                <div class="mb-1"><span class="fw-bold">Fuel type: </span>{{car.fuel_type}}</div>
                 <div class="mb-3"><span class="fw-bold">Price: </span>{{`${car.price}/h`}}</div>
                 <router-link class="btn btn-primary w-100" :to="`/view/${car.id}`">View</router-link>
             </div>
