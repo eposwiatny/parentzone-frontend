4ec15cafc7823fd9fe65492fa77214387c68ebb2
Eposwiatny <eposwiatny@gmail.com>
2022-03-27 12:52:22 +0200

    View car page, displaying data from API

--- src/views/cars/view/ViewCar.vue
+++ src/views/cars/view/ViewCar.vue
@@ -7,17 +7,16 @@
                 <div class="row">
 
                 <div class="col-6">
-                    <img class="img-thumbnail" src="https://ag-spots-2021.o.auroraobjects.eu/2021/07/04/audi-rs5-sportback-b9-c916204072021112424_1.jpg?1625390676" alt="">
+                    <img class="img-thumbnail" :src="car.img_url" alt="">
                 </div>
 
-
                 <div class="col-6">
-                <div class="mb-1"><span class="fw-bold">Brand: </span>Audi</div>
-                <div class="mb-1"><span class="fw-bold">Model: </span>A4</div>
-                <div class="mb-1"><span class="fw-bold">Places: </span>4 places</div>
-                <div class="mb-1"><span class="fw-bold">Transmission: </span>Automatic</div>
-                <div class="mb-1"><span class="fw-bold">Fuel type: </span>Gasoil</div>
-                <div class="mb-3"><span class="fw-bold">Price: </span>156/h</div>
+                <div class="mb-1"><span class="fw-bold">Brand: </span>{{car.car_model.car_brand.name}}</div>
+                <div class="mb-1"><span class="fw-bold">Model: </span>{{car.car_model.name}}</div>
+                <div class="mb-1"><span class="fw-bold">Places: </span>{{car.places}} places</div>
+                <div class="mb-1"><span class="fw-bold">Transmission: </span>{{car.is_automatic}}</div>
+                <div class="mb-1"><span class="fw-bold">Fuel type: </span>{{car.fuel_type}}</div>
+                <div class="mb-3"><span class="fw-bold">Price: </span>{{car.price}}/h</div>
                 <div class="mb-3"><span class="fw-bold">Description: </span>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo itaque non nulla vitae eveniet, blanditiis natus et omnis animi officia minus repellat. Minima aperiam corporis quaerat voluptas. Repellat, quaerat corrupti?</p>
                 </div>
@@ -32,3 +31,42 @@
             </div>
     </div>
 </template>
+<script lang="js">
+import axios from 'axios'
+export default{
+
+    name: 'view-car',
+
+    data(){
+        return{
+            car: {
+                id: null,
+                img_url: '',
+                places: '',
+                is_automatic: '',
+                fuel_type: '',
+                price: null,
+                car_model: {
+                    name: '',
+                    car_brand: {
+                        name: ''
+                    }
+                }
+            }
+        }
+    },
+
+    methods:{
+        loadData(){
+            axios.get('http://192.168.0.42:8000/api/cars/1').then( response => {
+                this.car = response.data.car
+            })
+        }
+    },
+
+    mounted(){
+        this.loadData();
+    }
+
+}
+</script>
